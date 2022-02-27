4af66693f4cbe170961550622344e550322abcaf
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-02-26 18:29:27 -0800

    fixed code and links for license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,85 +1,52 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license === "Apache") {
-    return "[![License: Apache](https://img.shields.io/badge/License-Apache-blue)](https://www.apache.org/licenses/LICENSE-2.0)";
-  } else if (license === "BSD 3") {
-    return "[![License: BSD 3](https://img.shields.io/badge/License-BSD%203-green)](https://opensource.org/licenses/BSD-3-Clause)";
-  } else if (license === "BSD 2") {
-    return "[![License: BSD 2](https://img.shields.io/badge/License-BSD%202-yellow)](https://opensource.org/licenses/BSD-2-Clause)";
-  } else if (license === "MIT") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-important)](https://opensource.org/licenses/MIT)";
-  } else if (license === "GPL") {
-    return "[![License: GPL](https://img.shields.io/badge/License-GPL-blueviolet)](https://www.gnu.org/licenses/gpl-3.0.en.html)";
-  } else if (license === "LGPL") {
-    return "[![License: LGPL](https://img.shields.io/badge/License-LGPL-ff69b4)](https://www.gnu.org/licenses/lgpl-3.0.en.html)";
-  } else if (license === "MPL") {
-    return "[![License: MPL](https://img.shields.io/badge/License-MPL-red)](https://www.mozilla.org/en-US/MPL/)";
-  } else if (license === "CDDL") {
-    return "[![License: CDDL](https://img.shields.io/badge/License-CDDL-yellowgreen)](https://opensource.org/licenses/CDDL-1.0)";
+
+let licenseBadge = '';
+
+function renderLicense(data) {
+  for (const license of data.license) {
+    if (license == "Apache-2.0") {
+      licenseBadge += `[![License: Apache-2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    } else if (license == "BSD 3") {
+      licenseBadge += `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+    } else if (license == "BSD 2") {
+      licenseBadge += `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`
+    } else if (license == "MIT") {
+      licenseBadge += `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    } else if (license == "GPLv3") {
+      licenseBadge += `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    } else if (license == "LGPLv3") {
+      licenseBadge += `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0`
+    } else if (license == "MPL 2.0") {
+      licenseBadge += `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    } else if (license == "CDDL 1.0") {
+      licenseBadge += `[![License: CDDL](https://img.shields.io/badge/License-CDDL-yellowgreen)](https://opensource.org/licenses/CDDL-1.0)`
+    }
   }
+  return licenseBadge;
 }
-
-module.exports = {
-  renderLicenseBadge: renderLicenseBadge
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  switch (license) {
-    case "Apache":
-      return "https://opensource.org/licenses/Apache-2.0";
-    
-    case "BSD 3":
-      return "https://opensource.org/licenses/BSD-3-Clause";
-    
-    case "BSD 2":
-      return "https://opensource.org/licenses/BSD-2-Clause";
-    
-    case "MIT":
-      return "https://opensource.org/licenses/MIT";
-    
-    case "GPL":
-      return "https://opensource.org/licenses/gpl-license";
-    
-    case "LGPL":
-      return "https://opensource.org/licenses/lgpl-license";
-    
-    case "MPL":
-      return "https://opensource.org/licenses/MPL-2.0";
-    
-    case "CDDL":
-      return "https://opensource.org/licenses/CDDL-1.0";
-    
-    default:
-      return "https://opensource.org/licenses";
-  }
-}
-
-module.exports = {
-  renderLicenseLink: renderLicenseLink
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-//function renderLicenseSection(license) {}
-
 
 // TODO: Create a function to generate markdown for README
+// User input on the array of questions are added on the following README sections: Title, Description, Installation, Usage, Contributing, Tests, License, and Questions.
+// When the links in the Table of Contents are clicked, the user is taken to the corresponding README section.
+
 function generateMarkdown(data) {
   return `
   
   # ${data.title}
 
+  ${renderLicense(data)}
+
+  ---
+  ## Description
+
   ${data.description}
 
-  ${data.renderLicenseBadge}
+
 
   [A deployed version can be viewed here.](${data.url})
 
   ---
-
   ## Table of Contents
   1. [Title](#title)
   2. [Description](#description)
@@ -129,10 +96,10 @@ function generateMarkdown(data) {
   ---
   ## Questions
   For questions or issues, please contact: 
-  - ${data.questions}
-  - GitHub Username: ${data.username}
+  - ${data.questions} 
   - Email: ${data.email}
-
+  - GitHub Username: ${data.username}
+  - GitHub Profile: ${data.profile}
 
 `;
 }
